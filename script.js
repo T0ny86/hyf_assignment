6ed3509255be4c60aade35b002d46617782adbd9
Tanous Atallah <tanous.atallah@gmail.com>
2021-11-22 14:52:07 +0100

    add footer + carousel initialization

---
+++ script.js
@@ -0,0 +1,9 @@
+const image=[];
+const interval = 700;
+
+image[0]= "https://i2-prod.mirror.co.uk/incoming/article1850849.ece/ALTERNATES/s1200d/Dortmund-vs-Hamburg.jpg";
+image[1]="https://i.guim.co.uk/img/media/117b87e6c44df446c256ff53a0a18a2f3cb20727/337_263_5534_3319/master/5534.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=4a54ec315beb0ec6302b285988bf1e89";
+image[2]="https://image.freepik.com/free-vector/covid-19-novel-coronavirus-latest-news-banner-template_1017-24548.jpg";
+image[3]="https://scd.infomigrants.net//media/resize/my_image_big/2a7632b04e27d617ccfa2b69f53541618bdcc13a.jpeg";
+
+
